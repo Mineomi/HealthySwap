32e2b2911a0d62fe61dc50d031ab2e05669f18c7
Mineomi <mineomi1234@gmail.com>
2023-11-19 18:57:39 +0100

    Kalkulator BMI

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  bmi : any
+  bmiRange : any
+
+  calculateBMI(height :any, weight : any){
+    this.bmi = weight / (height/100 * height/100)
+    this.bmi = this.round(this.bmi, 2)
+
+    switch(true){
+      case this.bmi<16: this.bmiRange = "Wygłodznie"; break;
+      case this.bmi<17: this.bmiRange = "Wychudzenie"; break;
+      case this.bmi<18.5: this.bmiRange = "Niedowaga"; break;
+      case this.bmi<25: this.bmiRange = "Pożądania masa ciała"; break;
+      case this.bmi<30: this.bmiRange = "nadwaga"; break;
+      case this.bmi<35: this.bmiRange = "otyłość 1 stopnia"; break;
+      case this.bmi<40: this.bmiRange = "otyłość 2 stopnia (duża)"; break;
+      case this.bmi>40: this.bmiRange = "otyłość 3 stopnia (chorobliwa)"; break;
+    }
+  }
+
+  round(num: number, fractionDigits: number): number {
+    return Number(num.toFixed(fractionDigits))
+}
 
   constructor() {}
 
